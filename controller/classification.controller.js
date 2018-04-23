684a269fd9d4a0d09bf068802a08d0b3e3936caa
vickyfan <vickyfan@67tech.cn>
2018-04-23 10:15:53 +0800

    修改注释说明

--- controller/classification.controller.js
+++ controller/classification.controller.js
@@ -2,10 +2,10 @@
  * @Author: VickyFan 
  * @Date: 2018-04-23 09:40:36 
  * @Last Modified by: VickyFan
- * @Last Modified time: 2018-04-23 09:43:32
+ * @Last Modified time: 2018-04-23 10:12:18
  */
 const DateModle = require('../models/classification.model');
-//增加课题
+//增加分类
 exports.create = function (req, res, next) {
   const dataModle = new DateModle(req.body);
   dataModle.save()
@@ -13,7 +13,7 @@ exports.create = function (req, res, next) {
       res.json(data);
     })
 };
-//删除某个课题
+//删除某个分类
 exports.remove = function (req, res, next) {
   const _id = req.params._id;
   DateModle.findByIdAndRemove(_id, function (err, data) {
@@ -24,7 +24,7 @@ exports.remove = function (req, res, next) {
     }
   })
 }
-//修改某个课题
+//修改某个分类
 exports.update = function (req, res, next) {
   var _id = req.params._id;
   DateModle.findByIdAndUpdate(_id, { $set: req.body }, { new: false })
@@ -32,7 +32,7 @@ exports.update = function (req, res, next) {
       res.json(data);
     })
 }
-//获取课题列表
+//获取分类列表
 exports.list = function (req, res, next) {
   var page = (req.body.page) ? req.body.page : 1;
   var rows = (req.body.rows) ? req.body.rows : 10;
--- routes/classification.js
+++ routes/classification.js
@@ -2,13 +2,13 @@
  * @Author: VickyFan 
  * @Date: 2018-04-23 09:44:52 
  * @Last Modified by: VickyFan
- * @Last Modified time: 2018-04-23 09:46:44
+ * @Last Modified time: 2018-04-23 10:07:46
  */
 var express = require('express');
 var router = express.Router();
 var dataCtrl = require('../controller/classification.controller');
-router.post('/ClassAdd', dataCtrl.create);//新增课题
-router.put('/ClassUpdate/:_id', dataCtrl.update);//修改个人信息
-router.delete('/ClassDel/:_id', dataCtrl.remove);//删除某个账户
+router.post('/ClassAdd', dataCtrl.create);//新增分类
+router.put('/ClassUpdate/:_id', dataCtrl.update);//修改某个分类
+router.delete('/ClassDel/:_id', dataCtrl.remove);//删除某个分类
 router.post('/ClassList', dataCtrl.list);//查询列表
 module.exports = router;
