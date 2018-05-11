047ba71510afff338cd8b6f27d381e26e7a32eb7
vickyfan <vickyfan@67tech.cn>
2018-05-11 13:25:34 +0800

    排版接口路由顺序

--- routes/subject.js
+++ routes/subject.js
@@ -1,16 +1,17 @@
 /*
  * @Author: VickyFan 
  * @Date: 2018-04-09 11:31:59 
- * @Last Modified by:   VickyFan 
- * @Last Modified time: 2018-04-09 11:31:59 
+ * @Last Modified by: VickyFan
+ * @Last Modified time: 2018-05-11 13:22:51
  */
 var express = require('express');
 var router = express.Router();
 var dataCtrl = require('../controller/subject.controller');
 router.post('/Add', dataCtrl.create);//新增课题
+router.delete('/DelSubject/:_id', dataCtrl.remove);//删除某个课题
+router.put('/Update/:_id', dataCtrl.update);//修改课题信息
 router.post('/List', dataCtrl.list);//查询课题列表
 router.get('/Search/:_id', dataCtrl.get);//查询某个课题详情
-router.delete('/DelSubject/:_id', dataCtrl.remove);//删除某个课题
 router.post('/Dels', dataCtrl.deletes);//删除多个课题
-router.put('/Update/:_id', dataCtrl.update);//修改课题信息
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,16 @@
  * @Author: VickyFan 
  * @Date: 2018-04-09 11:31:49 
  * @Last Modified by: VickyFan
- * @Last Modified time: 2018-04-25 09:46:04
+ * @Last Modified time: 2018-05-10 13:04:39
  */
 var express = require('express');
 var router = express.Router();
 var dataCtrl = require('../controller/user.controller');
 router.post('/AddUser', dataCtrl.create);//新增用户
+router.delete('/DeleteUser/:_id', dataCtrl.remove);//删除某个账户
+router.put('/UpdateInfo/:_id', dataCtrl.update);//修改个人信息
 router.post('/UserInfo', dataCtrl.get);//单个用户查询
 router.post('/CheckLogin', dataCtrl.checkLogin);//登录验证
-router.put('/UpdateInfo/:_id', dataCtrl.update);//修改个人信息
-router.delete('/DeleteUser/:_id', dataCtrl.remove);//删除某个账户
 router.post('/List', dataCtrl.list);//查询列表
 router.post('/deletes', dataCtrl.deletes);//删除多个
 module.exports = router;
